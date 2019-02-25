a23cb4354b159261b55cf5baa9dd79d0cfb92a59
Joanna Roczniak <jroczniak@smartrecruiters.com>
2019-02-25 18:43:22 +0100

    Try to add replaceWords function

--- content.js
+++ content.js
@@ -14,13 +14,26 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/chuj|dupa|kurwa/gi, function replacer(match){return dictionary[match];});
+            
+            var replacedText = text.replace(replaceWords(dictionary),function replacer(match){return dictionary[match];});
+            // var replacedText = text.replace(/chuj|dupa|kurwa/gi, function replacer(match){return dictionary[match];});
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
+}
+
+function replaceWords(dictionary){
+    let regularExpresion = "/";
+    let keys = Object.keys(dictionary);
+    keys.forEach(key => {
+        regularExpresion += key;
+        regularExpresion += "|";
+    });
+    regularExpresion = regularExpresion.slice(0, -1) + "/gi";
+    return regularExpresion;
 }
 
  
