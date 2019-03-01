0be59b33eaa2139914ce05f275b8d22b0d5a6c11
Joanna Roczniak <roczniak.joanna@gmail.com>
2019-03-01 19:26:57 +0100

    Wtyczka umozliwia uruchomienie zamieniacza zgodnie z zyczeniem.

--- popup.js
+++ popup.js
@@ -7,12 +7,16 @@ const dictionary = {
 
 document.getElementById("addButton").addEventListener("click", addToDictionary);
 
+document.getElementById("runButton").addEventListener("click", runDictionary);
+
 function addToDictionary() {
     const negativeWord = document.getElementById("negativeWord");
     const positiveWord = document.getElementById("positiveWord");
 
     dictionary[negativeWord.value] = positiveWord.value;
+}
 
+function runDictionary(){
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(tabs[0].id, { file: "content.js" }, () => {
             chrome.tabs.sendMessage(tabs[0].id, dictionary)
